f488a696164e25aa7810ade03cf5e795c9d47ad2
Aysel Ünlü <aysell.unlu48@gmail.com>
2023-01-26 00:46:24 +0300

    basics of accordion

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,19 +1,156 @@
 import React from "react";
-import { Image } from "react-bootstrap";
+import { Image, Accordion } from "react-bootstrap";
 import Software from "../img/software.png";
-import '../Styles/Projects.css';
+import "../Styles/Projects.css";
+import "../Styles/Accordion.css";
 
 const Projects = () => {
   return (
     <div className="container-fluid ">
-      <div className="project-title pb-4 pt-4">OUR WORK & PROJECTS</div>
-      <div className="project-second-title">
-        Web / Mobile Application Development
-      </div>
+      <div className="project-title pb-4 pt-4 mb-4">OUR WORK & PROJECTS</div>
 
-      <div className="row mt-4 mb-4">
-        <div className="col-lg-4 col-sm-12">
+      <div className="row mt-4 mb-4 pb-5">
+        <Accordion
+        defaultActiveKey="0"
+        >
+          <Accordion.Item eventKey="0">
+            <Accordion.Header className="project-second-title">
+              Web / Mobile Application Development
+            </Accordion.Header>
+            <Accordion.Body className="project-text">
+              <div className="row">
+                <div className="col-4">
+                  <Image src={Software} width={450} />
+                </div>
+                <div className="col-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header className="project-second-title">
+              UI Design
+            </Accordion.Header>
+            <Accordion.Body className="project-text">
+              <div className="row">
+                <div className="col-4">
+                  <Image src={Software} width={450} />
+                </div>
+                <div className="col-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header className="project-second-title">
+              Logo - Corporate Identity Design
+            </Accordion.Header>
+            <Accordion.Body className="project-text">
+              <div className="row">
+                <div className="col-4">
+                  <Image src={Software} width={450} />
+                </div>
+                <div className="col-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header className="project-second-title">
+              Social Media Management
+            </Accordion.Header>
+            <Accordion.Body className="project-text">
+              <div className="row">
+                <div className="col-4">
+                  <Image src={Software} width={450} />
+                </div>
+                <div className="col-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="4">
+            <Accordion.Header className="project-second-title">
+              Photography - Photo Edit
+            </Accordion.Header>
+            <Accordion.Body className="project-text">
+              <div className="row">
+                <div className="col-4">
+                  <Image src={Software} width={450} />
+                </div>
+                <div className="col-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="5">
+            <Accordion.Header className="project-second-title">
+              Videography - Video Edit
+            </Accordion.Header>
+            <Accordion.Body className="project-text">
+              <div className="row">
+                <div className="col-4">
+                  <Image src={Software} width={450} />
+                </div>
+                <div className="col-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </div>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        {/* <div className="col-lg-4 col-sm-12">
           <Image src={Software} width={450} />
+        </div> */}
+        {/* <div className="project-second-title">
+          Web / Mobile Application Development
         </div>
         <div className="col-lg-8 col-sm-12">
           <div className="project-text">
@@ -79,7 +216,7 @@ const Projects = () => {
           </div>
         </div>
       </div>
-      <div className="project-second-title">Photography</div>
+      <div className="project-second-title">Photography - Photo Edit</div>
       <div className="row mt-4 mb-4">
         <div className="col">
           <div className="project-text">
@@ -125,7 +262,7 @@ const Projects = () => {
             and more recently with desktop publishing software like Aldus
             PageMaker including versions of Lorem Ipsum.
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
